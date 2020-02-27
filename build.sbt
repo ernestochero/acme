1d38c8be433c4b383587cea8d8195d2b61be8448
Ernesto Chero <ernesto.chero.contractor@bbva.com>
2020-02-27 17:49:43 -0500

    Adding haddop library to read in hdfs and assembly configuration

--- build.sbt
+++ build.sbt
@@ -15,4 +15,25 @@ libraryDependencies ++= Seq(
   "com.github.pureconfig" %% "pureconfig" % "0.12.1",
   "ch.qos.logback" % "logback-classic" % "1.2.3",
   "org.log4s" %% "log4s" % "1.8.2",
+  "org.apache.hadoop" % "hadoop-hdfs" % "3.2.1"
 )
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) =>
+    xs map {_.toLowerCase} match {
+      case "manifest.mf" :: Nil | "index.list" :: Nil | "dependencies" :: Nil =>
+        MergeStrategy.discard
+      case ps @ x :: xs if ps.last.endsWith(".sf") || ps.last.endsWith(".dsa") =>
+        MergeStrategy.discard
+      case "plexus" :: xs =>
+        MergeStrategy.discard
+      case "services" :: xs =>
+        MergeStrategy.filterDistinctLines
+      case "spring.schemas" :: Nil | "spring.handlers" :: Nil =>
+        MergeStrategy.filterDistinctLines
+      case _ => MergeStrategy.first
+    }
+  case "application.conf" => MergeStrategy.concat
+  case "reference.conf" => MergeStrategy.concat
+  case _ => MergeStrategy.first
+}
--- src/main/scala/AcmeModule.scala
+++ src/main/scala/AcmeModule.scala
@@ -13,9 +13,21 @@ object Pattern {
   val stagingRawPattern: Regex = "\\d{8}".r
   val masterPattern: Regex     = "(\\d{1,4}([.\\-])\\d{1,2}([.\\-])\\d{1,4})".r
 }
+
 object AcmeModule {
   import Pattern._
   case class Acme() {
+    /*    private def readHDFSDirectory(strPath: String): Option[List[FileStatus]] = {
+      val directoryPath = new Path(strPath)
+      val fileSystem    = FileSystem.get(sc.hadoopConfiguration)
+      if (fileSystem.isDirectory(directoryPath))
+        Some(fileSystem.listStatus(directoryPath).toList)
+      else None
+    }
+    private def processHDFSDirectory(stage: List[FileStatus],
+                                     pattern: Regex): List[Option[String]] =
+      stage.map(_.getPath.getName).map(name => pattern findFirstIn name)*/
+
     private def readDirectory(path: String): Option[File] = {
       val file = new File(path)
       if (file.isDirectory) Some(file) else None
@@ -36,18 +48,39 @@ object AcmeModule {
       rawCount == masterCount
     }
 
+    def createStageObject(csvLine: String): Stage = {
+      val lineContent = csvLine.split(",").map(_.trim)
+      Stage(lineContent(0), lineContent(1), lineContent(2))
+    }
+
     def readCSV(csvPath: String): List[Stage] = {
       import scala.io._
       val csvFile = Source.fromFile(csvPath)
       val csvContent = csvFile
         .getLines()
-        .map { line =>
-          val lineContent = line.split(",").map(_.trim)
-          Stage(lineContent(0), lineContent(1), lineContent(2))
-        }
+        .map(createStageObject)
         .toList
       csvContent
     }
+
+    /*    def executeProcessHDFS(stagingPath: String, rawPath: String, masterPath: String): Unit = {
+      val stagingHDFSDirectory: Option[List[FileStatus]] = readHDFSDirectory(stagingPath)
+      val rawHDFSDirectory: Option[List[FileStatus]]     = readHDFSDirectory(rawPath)
+      val masterHDFSrDirectory: Option[List[FileStatus]] = readHDFSDirectory(masterPath)
+      val result = for {
+        stagingHDFS <- stagingHDFSDirectory
+        rawHDFS     <- rawHDFSDirectory
+        masterHDFS  <- masterHDFSrDirectory
+        resultStagingHDFS = processHDFSDirectory(stagingHDFS, stagingRawPattern)
+        resultRawHDFS     = processHDFSDirectory(rawHDFS, stagingRawPattern)
+        resultMasterHDFS  = processHDFSDirectory(masterHDFS, masterPattern)
+      } yield validateDirectories(resultStagingHDFS, resultRawHDFS, resultMasterHDFS)
+      if (result.getOrElse(false)) {
+        AcmeLogger.logger.info("Success!! we have the same number of elements")
+      } else {
+        AcmeLogger.logger.error("An error occurred")
+      }
+    }*/
 
     def executeProcess(stagingPath: String, rawPath: String, masterPath: String): Unit = {
       val stagingDirectory: Option[File] = readDirectory(stagingPath)
